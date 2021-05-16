9e60808b846798aa2862bf5a5e4fc7773c8b4e9a
Raymond-Neo <s10185503@connect.np.edu.sg>
2021-05-16 19:09:00 +0800

    update webhook

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from telegram.ext import *
 import Responses as r
 import Memory_Responses
 import requests
-import flask
+
 import os
 
-server = flask(__name__)
+
 def start_command(update, context):
     update.effective_message.reply_text("Hello baby\n"
                                         + "Note: Always /start to return to top\n"
@@ -140,10 +140,9 @@ def main():
 
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path='1712123477:AAEVFUInHP9V7eYkedBqieNpUu4acp9cMYo')
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + '1712123477:AAEVFUInHP9V7eYkedBqieNpUu4acp9cMYo'
-                           )
-    updater.idle()
+                          url_path='1712123477:AAEVFUInHP9V7eYkedBqieNpUu4acp9cMYo',
+                          webhook_url='https://yourherokuappname.herokuapp.com/' + '1712123477:AAEVFUInHP9V7eYkedBqieNp'
+                                      + 'Uu4acp9cMYo')
 
 
 main()
